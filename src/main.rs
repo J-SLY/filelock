0453b7f595998645713aa0d54b681444222d9eca
JSLY <2029872426@qq.com>
2026-06-19 22:59:14 +0800

    注册子指令

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,34 @@
 use std::{cmp::Ordering, fs, process};
-use clap::Parser;
+use clap::{Parser, Subcommand};
 
+
+/// Gat Args 
+/// 
+/// `filename:String`,`offset:usize`,`is_mod:bool`
 #[derive(Parser,Debug)]
 #[command(author,version,about,long_about=None)]
 struct Args{
-
-    #[arg(short,long)]
-    filename:String,
-
-    #[arg(short,long)]
-    offset:usize,
-
-    #[arg(short)]
-    m:bool,
+    #[command(subcommand)]
+    command:Commands,
+    
 }
+#[derive(Subcommand, Debug)]
+enum Commands{
+    Lock{
+        #[arg(short,long)]
+        filename:String,
+
+        #[arg(short,long)]
+        offset:usize,
+
+        #[arg(short)]
+        m:bool,
+    }
+}
+/// # Example
+/// using **`let data:Vex<u8> = read_file(&file_name);`**
+/// to get file data, return a Vec\<u8\>.
+/// 
 fn read_file(filename:&String)->Vec<u8>{
     match fs::read(filename){
         Ok(data) => {
@@ -31,6 +46,12 @@ fn read_file(filename:&String)->Vec<u8>{
         }
     }
 }
+/// # Example
+/// using **`let key = check_offset(offset,&data,is_mod);`** to get right offset.
+/// 
+/// if `is_mod` is false and offset is greater than length of data, this function will throw an error and exit.
+///
+/// if `is_mod` is true, this function will return right offset.
 fn check_offset(offset:usize,data:&Vec<u8>,is_mod:bool)->usize{
     match is_mod{
         true => offset%data.len(),
@@ -45,6 +66,7 @@ fn check_offset(offset:usize,data:&Vec<u8>,is_mod:bool)->usize{
         }
     }
 }
+
 fn file_lock(data:&mut Vec<u8>,offset:u8){
     for byte in data.iter_mut() {
         *byte ^= offset;
@@ -60,16 +82,22 @@ fn write_file(data:&Vec<u8>,filename:&String){
     }
 }
 fn main() {
-    let mut args = Args::parse();
-    let data = read_file(&args.filename);
+    let args = Args::parse();
+    match args.command {
+        Commands::Lock { filename, offset, m } =>{
+            let data = read_file(&filename);
 
-    args.offset = check_offset(args.offset, &data,args.m);
+        let offset = check_offset(offset, &data,m);
+        
+        let key = (offset % 256) as u8;
+        let mut data = data;
+
+        file_lock(&mut data,key);
+        write_file(&data, &filename);
+
+        println!("文件{}已关于密钥{}异或",filename,key);
+        
+        }
+    }
     
-    let key = (args.offset % 256) as u8;
-    let mut data = data;
-
-    file_lock(&mut data,key);
-    write_file(&data, &args.filename);
-
-    println!("文件{}已关于密钥{}异或",args.filename,key);
 }
